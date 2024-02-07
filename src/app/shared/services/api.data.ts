8070ab746a8df77ea2957979d56f359d34c48932
ruvimbo_mambinge <ruemee93@gmail.com>
2024-02-07 10:08:33 +0200

    Stash changes

--- src/app/shared/services/api.data.ts
+++ src/app/shared/services/api.data.ts
@@ -5,13 +5,14 @@ export enum API {
   // SERVICE='https://ba79-68-178-203-55.ngrok-free.app/',
   // CLIENTS ="https://035b-68-178-203-55.ngrok-free.app/",
   // CLIENTS ="http://192.168.10.24:8991/",
-  // SERVICE="http://68.178.203.55:8989/",
-  // CLIENTS ="http://68.178.203.55:8991/",
-  // PAYMENTS ='http://68.178.203.55:8990/'
+  
+  SERVICE="http://68.178.203.55:8989/",
+  CLIENTS ="http://68.178.203.55:8991/",
+  PAYMENTS ='http://68.178.203.55:8990/'
 
 
-  SERVICE="https://5aaa-68-178-203-55.ngrok-free.app/api/parameters-service/",
-  CLIENTS ="https://5aaa-68-178-203-55.ngrok-free.app/api/kyc-service/",
-  PAYMENTS ='https://5aaa-68-178-203-55.ngrok-free.app/api/payment-service/'
+  // SERVICE="https://5aaa-68-178-203-55.ngrok-free.app/api/parameters-service/",
+  // CLIENTS ="https://5aaa-68-178-203-55.ngrok-free.app/api/kyc-service/",
+  // PAYMENTS ='https://5aaa-68-178-203-55.ngrok-free.app/api/payment-service/'
 
 }
