9a181abb7bb66ec46d51c66bb12acefd8b3bd254
ruvimbo_mambinge <ruvimbom@afrosoftzw.onmicrosoft.com>
2024-07-01 12:12:45 +0200

    url-push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl:"http://192.168.10.39:8989/"
+  // baseUrl:"http://192.168.10.39:8989/"
+  baseUrl:"https://b807-197-211-207-230.ngrok-free.app/"
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // baseUrl:"https://22ce-68-178-203-55.ngrok-free.app/"
-  baseUrl:"http://192.168.10.86:8989/"
+  baseUrl:"https://b807-197-211-207-230.ngrok-free.app/"
+  // baseUrl:"http://192.168.10.86:8989/"
 
 };
 
