94dbb0a984b65b18bfcb38fe18c3ec8758806f01
ruvimbo_mambinge <ruvimbom@afrosoftzw.onmicrosoft.com>
2024-07-23 16:09:30 +0200

    api

--- src/app/shared/services/api.data.ts
+++ src/app/shared/services/api.data.ts
@@ -1,9 +1,9 @@
 export enum API {
   // SERVICE='http://192.168.10.24:8986/api/parameters-service/',
   // SERVICE='https://192.168.10.24/',
-  SERVICE="https://af98-197-211-207-230.ngrok-free.app/api/parameters-service/",
-  CLIENTS ="https://af98-197-211-207-230.ngrok-free.app/api/kyc-service/",
-  PAYMENTS ='https://af98-197-211-207-230.ngrok-free.app/api/payment-service/'
+  SERVICE="https://0edb-197-211-207-230.ngrok-free.app/api/parameters-service/",
+  CLIENTS ="https://0edb-197-211-207-230.ngrok-free.app/api/kyc-service/",
+  PAYMENTS ='https://0edb-197-211-207-230.ngrok-free.app/api/payment-service/'
 
   // SERVICE='https://ba79-68-178-203-55.ngrok-free.app/',
   // CLIENTS ="https://035b-68-178-203-55.ngrok-free.app/",
